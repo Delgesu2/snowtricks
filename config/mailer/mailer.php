345c928c161a2788a65e87aad4a1e7d9466b0aa5
Xavier <xavierus70@hotmail.com>
2020-02-24 13:12:50 +0100

    Delete mailer.php

--- config/mailer/mailer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'smtp'     => 'fajitas.o2switch.net',
-    'username' => 'contact@devxdemo.eu',
-    'password' => 'Ygn429FsHqns',
-    'from'     => 'contact@devxdemo.eu',
-    'to'       => 'coutant.xavier@orange.fr'
-];
